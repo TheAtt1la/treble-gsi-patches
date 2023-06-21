dc11e5a16c046444e73ef44e26cce7d7cb31614a
User name <132944870+TheAtt1la@users.noreply.github.com>
2023-06-21 00:19:18 -0300

    Update pex.mk

--- pex.mk
+++ pex.mk
@@ -5,7 +5,6 @@ $(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
 
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-SELINUX_IGNORE_NEVERALLOWS := true
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
@@ -15,8 +14,3 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 EXTRA_UDFPS_ANIMATIONS := true
 FACE_UNLOCK_SUPPORTED := true
-
-PRODUCT_PACKAGES += \
-    libaptX_encoder \
-    libaptXHD_encoder
-
